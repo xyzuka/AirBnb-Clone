a7b05d1c6e30de8d3f7cc1d9822cfd6dd984f35f
Xyzuka <yeebrandon@outlook.com>
2022-09-02 09:59:55 -0700

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,7 +11,7 @@ export default function Card(props) {
     return (
       <div className="card">
         {badgeText && <div className="card-badge">{badgeText}</div>}
-        <img src={`../../public/assets/${props.items.coverImg}`} className="card-image" />
+        <img src={`/assets/${props.items.coverImg}`} className="card-image" />
         <div className="card-stats">
             <img src="../../public/assets/star.png" className="card-star" />
             <span>{props.rating}</span>
